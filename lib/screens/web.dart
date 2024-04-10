6c23ff589a673d2d7459c52fcaabae29f2aeff60
Enzo De Simone <en.desimone@outlook.it>
2024-04-10 11:35:02 +0200

    fix

--- lib/screens/web.dart
+++ lib/screens/web.dart
@@ -1,5 +1,11 @@
 // ignore: avoid_web_libraries_in_flutter
+import 'dart:developer';
 import 'dart:html' as html;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:js/js.dart';
+import 'package:js/js_util.dart' as js_util;
+
 import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
@@ -7,6 +13,20 @@ import 'package:simple_barcode_scanner/constant.dart';
 import 'package:simple_barcode_scanner/enum.dart';
 
 /// Barcode scanner for web using iframe
+///
+///
+///
+
+@JS('getAvailableCameras')
+external dynamic getAvailableCameras();
+
+@JS('switchCamera')
+external void switchCamera(String id);
+
+@JS('Html5Qrcode.getCameras')
+external void getCameras();
+
+///
 class BarcodeScanner extends StatelessWidget {
   final String? lineColor;
   final String? cancelButtonText;
@@ -15,6 +35,7 @@ class BarcodeScanner extends StatelessWidget {
   final Function(String) onScanned;
   final String? appBarTitle;
   final bool? centerTitle;
+
   const BarcodeScanner({
     Key? key,
     this.lineColor,
@@ -28,7 +49,7 @@ class BarcodeScanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String createdViewId = DateTime.now().microsecondsSinceEpoch.toString();
+    String createdViewId = '1';
     String? barcodeNumber;
 
     final html.IFrameElement iframe = html.IFrameElement()
@@ -50,8 +71,55 @@ class BarcodeScanner extends StatelessWidget {
     ui.platformViewRegistry
         .registerViewFactory(createdViewId, (int viewId) => iframe);
 
-    return HtmlElementView(
-      viewType: createdViewId,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          FutureBuilder(
+              future: getCamerasID(),
+              builder: (ctx, AsyncSnapshot<List<String>> snapshot) {
+                if (snapshot.connectionState != ConnectionState.done) {
+                  return Container();
+                }
+
+                return Row(
+                  children: snapshot.data!
+                      .map(
+                        (e) => ElevatedButton(
+                            onPressed: () async {
+                              switchCamera(e);
+                            },
+                            child: Text(e.substring(0, 6))),
+                      )
+                      .toList(),
+                );
+              }),
+          ElevatedButton(
+              onPressed: () async {
+                getCameras();
+              },
+              child: const Text('asdsda')),
+          const SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            height: 400,
+            width: 400,
+            child: HtmlElementView(
+              viewType: createdViewId,
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  Future<List<String>> getCamerasID() async {
+    try {
+      final result = await js_util.promiseToFuture(getAvailableCameras());
+      return result.toString().split('@').toList();
+    } catch (e) {
+      log('Camera error: $e');
+      return [];
+    }
   }
 }
